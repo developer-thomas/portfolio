87bffa9937cb0ceaad90126323c23db398cab6d8
developer-thomas <andepipous@gmail.com>
2024-03-03 19:00:10 -0400

    feature: created a json file to save the skills data and the skills component can consume the data using httpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AboutComponent } from './pages/about/about.component';
 import { HabilidadesComponent } from './pages/habilidades/habilidades.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,13 +17,10 @@ import { HabilidadesComponent } from './pages/habilidades/habilidades.component'
     HeaderComponent,
     FooterComponent,
     AboutComponent,
-    HabilidadesComponent
+    HabilidadesComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
