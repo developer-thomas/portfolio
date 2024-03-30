d3d7789fdc23c56ab5f46618fe01d65d126d2023
developer-thomas <andepipous@gmail.com>
2024-03-30 13:40:09 -0400

    fix: fix reponsivity to homepage

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,30 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var VANTA:any;
+declare var VANTA: any;
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
-
   ngOnInit(): void {
     VANTA.GLOBE({
       el: '#vanta',
       mouseControls: true,
       touchControls: true,
       gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00,
-      scale: 1.00,
-      scaleMobile: 1.00,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
       showLines: false,
-      backgroundColor: 0    
-    })
+      backgroundColor: 0,
+    });
   }
-
-  
-
-
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,44 +1,32 @@
 import { Component, AfterViewInit } from '@angular/core';
 
-declare var VANTA:any;
+declare var VANTA: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss', './home.responsive.scss'],
 })
-export class HomeComponent implements AfterViewInit{
-  public linkedin: string = 'https://www.linkedin.com/in/thomas-edson-dev/'
-  public github: string = 'https://github.com/developer-thomas'
-  public instagram: string = 'https://www.instagram.com/thomas.devp/'
+export class HomeComponent implements AfterViewInit {
+  public linkedin: string = 'https://www.linkedin.com/in/thomas-edson-dev/';
+  public github: string = 'https://github.com/developer-thomas';
+  public instagram: string = 'https://www.instagram.com/thomas.devp/';
 
+  constructor() {}
 
-  constructor() { }
-  
-   // you can also use OnInit hooks
-   ngAfterViewInit(): void {
-  //   VANTA.NET({
-  //     el: "#vanta",
-  //     color: 0xca6c56,
-  //     backgroundColor: 0x342071,
-  //     points: 10.00,
-  //     maxDistance: 15.00,
-  //     spacing: 20.00
-  // })
-
-      VANTA.DOTS({
-        el: '#vanta',
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00,
-        showLines: false,
-        backgroundColor: 0o3
-      })
-      
-    }
-} 
-
+  // you can also use OnInit hooks
+  ngAfterViewInit(): void {
+    VANTA.GLOBE({
+      el: '#vanta',
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      showLines: false,
+      backgroundColor: 0,
+    });
+  }
+}
